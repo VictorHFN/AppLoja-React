dbaa0df68ac475f6dc5fe52e0af2600e70fb6737
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-04-11 17:13:10 -0300

    Banco de dados corrigido

--- services/connectionFirebase.js
+++ services/connectionFirebase.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBZByYQUqjbIPCXYIJHk7TjICCM-h6ehYQ",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
